30a7ca1086312f6ca077a30214b984c1da892df5
Maedeh Zebhi <maedehzebhi@gmail.com>
2023-04-03 15:49:08 +0430

    Added mongoose package and Connect to DB

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+//config environment variables in .env
 dotenv.config({ path: './config.env' });
 
+//connect to database
+const DB = process.env.DATABASE_LOCAL;
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('db connection successful!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//app running
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
